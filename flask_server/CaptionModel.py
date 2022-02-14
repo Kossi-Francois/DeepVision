676a7062d17386c0f5f1a8a8be61028a9b0e3c2d
Kossi-Francois <53384107+Kossi-Francois@users.noreply.github.com>
2022-02-14 18:56:41 +0100

    add caption model

--- flask_server/CaptionModel.py
+++ flask_server/CaptionModel.py
@@ -102,6 +102,118 @@ def load_image_phoneCam(img):
 
 
 
+
+
+class BahdanauAttention(tf.keras.Model):
+  def __init__(self, units):
+    super(BahdanauAttention, self).__init__()
+    self.W1 = tf.keras.layers.Dense(units) #w encoder
+    self.W2 = tf.keras.layers.Dense(units) #w decoder hidden
+    self.V = tf.keras.layers.Dense(1) #w alignement
+
+  def call(self, features, hidden):
+    # features(CNN_encoder output) shape == (batch_size, 64, embedding_dim)
+
+    # hidden shape == (batch_size, hidden_size)
+    # hidden_with_time_axis shape == (batch_size, 1, hidden_size)
+    hidden_with_time_axis = tf.expand_dims(hidden, 1)
+
+    # attention_hidden_layer shape == (batch_size, 64, units)
+    attention_hidden_layer = (tf.nn.tanh(self.W1(features) +
+                                         self.W2(hidden_with_time_axis)))
+
+    # score shape == (batch_size, 64, 1)
+    # This gives you an unnormalized score for each image feature.
+    score = self.V(attention_hidden_layer) #alignement scores
+
+    # attention_weights shape == (batch_size, 64, 1)
+    attention_weights = tf.nn.softmax(score, axis=1)
+
+    # context_vector shape after sum == (batch_size, hidden_size)
+    context_vector = attention_weights * features
+    context_vector = tf.reduce_sum(context_vector, axis=1)
+
+    return context_vector, attention_weights
+
+
+
+
+
+
+class CNN_Encoder(tf.keras.Model):
+    # Since you have already extracted the features and dumped it using pickle
+    # This encoder passes those features through a Fully connected layer
+    def __init__(self, embedding_dim):
+        super(CNN_Encoder, self).__init__()
+        # shape after fc == (batch_size, 64, embedding_dim)
+        self.fc = tf.keras.layers.Dense(embedding_dim)
+
+    def call(self, x):
+        x = self.fc(x)
+        x = tf.nn.relu(x)
+        return x
+
+
+
+
+
+class RNN_Decoder(tf.keras.Model):
+  def __init__(self, embedding_dim, units, vocab_size):
+    super(RNN_Decoder, self).__init__()
+    self.units = units
+
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+
+    self.gru = tf.keras.layers.LSTM(self.units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+    #self.gru = tf.keras.layers.Bidirectional(self.lstm, merge_mode='sum')
+
+    self.fc1 = tf.keras.layers.Dense(self.units)
+    self.fc2 = tf.keras.layers.Dense(vocab_size)
+
+    self.attention = BahdanauAttention(self.units)
+    self.drop =  tf.keras.layers.Dropout(0.35)
+
+  def call(self, inputs, features, hidden, training = True):
+
+    hidden_h,  hidden_c = hidden[0], hidden[1]
+
+    # defining attention as a separate model
+    context_vector, attention_weights = self.attention(features, hidden_h)
+
+    # x shape after passing through embedding == (batch_size, 1, embedding_dim)
+    x = self.embedding(inputs)
+
+    # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
+    x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+
+    r_hidden_h = tf.zeros((x.shape[0], self.units))
+
+    # passing the concatenated vector to the GRU
+    output, state_h, state_c = self.gru(x, initial_state=[r_hidden_h,  hidden_c])
+
+    # shape == (batch_size, max_length, hidden_size)
+    x = self.fc1(output)
+
+    # x shape == (batch_size * max_length, hidden_size)
+    x = tf.reshape(x, (-1, x.shape[2]))
+
+    #add dropout
+    x = self.drop(x, training= training)
+
+    # output shape == (batch_size * max_length, vocab)
+    x = self.fc2(x)
+
+    return x, [state_h, state_c], attention_weights
+
+  def reset_state(self, batch_size):
+    return [tf.zeros((batch_size, self.units)), tf.zeros((batch_size, self.units))]
+
+
+
+
 ############
 
 
